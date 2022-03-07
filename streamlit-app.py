51abb91a839e46ce89008f8d98997ad18e3e4c64
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2022-03-07 13:29:34 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -19,7 +19,7 @@ def execute(cmd):
 
 # Example
 for path in execute("git clone https://github.com/fsquillace/junest.git ~/.local/share/junest"):
-    print(path, end="")
+    st.write(path, end="")
 # print("-------")
 # result = subprocess.run("git clone https://github.com/fsquillace/junest.git ~/.local/share/junest",shell=True)
 # print(result)
